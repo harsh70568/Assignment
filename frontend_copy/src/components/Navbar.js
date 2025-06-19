025072baf87aec74df9d490d513d2fa3813dbafd
harsh.backend <harsh.2024it1034@kiet.edu>
2025-06-19 11:43:07 +0530

    fixed toggling

--- frontend_copy/src/components/Navbar.js
+++ frontend_copy/src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { ThemeContext } from '../contexts/ThemeContext';
-import ThemeToggle from './ThemeToggle';
 import './Navbar.css';
 
 const Navbar = () => {
@@ -13,7 +12,6 @@ const Navbar = () => {
         <Link to="/" className="navbar-brand">
           Student Progress
         </Link>
-        <ThemeToggle />
       </div>
     </nav>
   );
